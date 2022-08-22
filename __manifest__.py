edaff3ce0b250c244e28663a1b27842a226a9f1f
Ange Mollou <angemollou@outlook.com>
2022-08-22 10:56:06 +0000

    Add i18n via a during installation script

--- __manifest__.py
+++ __manifest__.py
@@ -2,34 +2,29 @@
 {
     'name': "odoo_testing",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Overide some translations of the `stock` module""",
 
     'description': """
-        Long description of module's purpose
+        Overide some translations of the `stock` module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "angemollou@outlook.com",
+    'website': "https://www.upwork.com/freelancers/~0155562fb4d538d083",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Localization',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'data/data.xml',
+        # Uncomment following line allows to create ir.translation records directly from a CSV
+        # if there are not duplicates
+        # 'data/ir.translation.csv',
     ],
 }
--- models/models.py
+++ models/models.py
@@ -1,18 +1,53 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from unittest import result
+from odoo import models, fields, api, _
+import odoo.addons.base.models.ir_translation as ir_translation
+import csv
+TRANSLATIONS_CSV = "./data/ir.translation.csv"
+TRANSLATIONS_MODULE = "stock"
+TRANSLATIONS_LANG = "fr_FR"
 
 
-# class odoo_testing(models.Model):
-#     _name = 'odoo_testing.odoo_testing'
-#     _description = 'odoo_testing.odoo_testing'
+class odoo_testing(models.Model):
+    _inherit = "stock.picking.type"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @classmethod
+    def read_csv(cls, filename):
+        translations = []
+        with open(filename, mode='r') as f:
+            reader = csv.reader(f)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                translations.append({
+                    "module": row[0],
+                    "type": row[1],
+                    "name": row[2],
+                    # "res_id": row[3],
+                    "src": row[4],
+                    "value": row[5],
+                    "comments": row[6],
+                    "lang": row[7],
+                    "state": row[8],
+                })
+        return translations
+
+    @api.model
+    def import_translation(self):
+        print("Importing FR translation for `stock` module...")
+        ir_translation = self.env['ir.translation']
+        trans_list = list(odoo_testing.read_csv(TRANSLATIONS_CSV))
+        result = []
+        for trans in trans_list:
+            domain = [
+                ("lang", "=", trans['lang']),
+                ("src", "=", trans['src']),
+                ("type", "=", trans['type']),
+                # ("name", "=", trans['name']),
+                # ("module", "=", trans['module']),
+            ]
+            record = ir_translation.search(domain)
+            # Easiest way to do is to update records, recreating (unlink & create) them is harder
+            result.append(record.write(trans))
+        print("Done!")
